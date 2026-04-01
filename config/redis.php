4c93ed16c0fa44b23a3a45f7986a00405870603b
Vladimir Runchev <vlatkorun@gmail.com>
2026-04-01 16:44:54 +0200

    chore(storage): remove old Redis classes superseded by namespace move
    RedisConnectionFactory and RedisRecordStorage under Infrastructure\Storage
    were replaced by their counterparts in Infrastructure\Storage\Redis in the
    previous refactor. config/redis.php was superseded by config/redis.yaml.

--- config/redis.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'host' => $_ENV['REDIS_HOST'] ?? 'localhost',
-    'port' => (int) ($_ENV['REDIS_PORT'] ?? 6379),
-    'database' => (int) ($_ENV['REDIS_DB'] ?? 0),
-];
